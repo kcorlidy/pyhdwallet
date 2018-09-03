2e86ff7a623aa82f36f6330120cbb7e6062da68a
kcorlidy <1220181099@qq.com>
2018-09-03 17:26:40 +0800

    build basical functions but need to rename somethingng

---
+++ Derivation_Path.py
@@ -0,0 +1,66 @@
+from bip32utils import BIP32Key,Base58
+from root_key import key
+from binascii import unhexlify,hexlify
+import sys
+
+class bip44(object):
+	def __init__(self, e = None , path = None):
+		self.e 				= key.generate(128).seed()
+		self.path 			= path
+		self.BIP32_HARDEN 	= 0x80000000
+		self.k 				= None
+
+	@staticmethod
+	def path(p):
+		p = p.split("/")
+		state = False if p[0].lower() != "m" or "44" not in p[1] else True 
+		#if false mean user gave an unexpected path
+		if state:
+			 return bip44(path=p)
+		else:
+			raise Exception("Path error:please give a correct path")
+
+	def Account(self):
+		#Account Extended Private/Public Key
+		pass
+
+	def address(self):
+		return self.k.Address()
+
+	def bip32ex_key(self):
+		k = BIP32Key.fromEntropy(self.e)
+		for p in self.path:
+			if "'" in p and p != 'm':
+				k = k.ChildKey(int(p.strip("'"))+self.BIP32_HARDEN)
+			elif p != 'm':
+				k = k.ChildKey(int(p))
+		self.k = k
+		return k.ExtendedKey(),k.ExtendedKey(private=False)
+
+	def key_pairs(self):
+		#Derived Private/Public Key
+		return self.b2h(self.k.PrivateKey()),self.b2h(self.k.PublicKey())
+
+	def wif(self):
+		return self.k.WalletImportFormat()
+
+	@property
+	def entropy(self):
+		return self.e
+
+	@entropy.setter
+	def entropy(self,e):
+		self.e = e
+
+	def b2h(self,b):
+		h = hexlify(b)
+		return h if sys.version < '3' else h.decode('utf8')
+
+if __name__ == '__main__':
+	bip = bip44.path("m/44'/0'/0'/0/0")
+	bip.entropy = key.to_mnemonic(data="a9495fe923ce601f4394c8a7adadabc3").seed()
+	print(
+		bip.bip32ex_key(),
+		bip.address(),
+		bip.key_pairs(),
+		bip.Private())
--- root_key.py
+++ root_key.py
@@ -13,13 +13,13 @@ class key(object):
 		return h if sys.version < '3' else h.decode('utf8')
 
 	@staticmethod
-	def gen_seed(data,lang="english"):
+	def to_mnemonic(data,lang="english"):
 		data = data if type(data) == bytes else bytes(data,"utf8")
 		m = key(m = mc(lang).to_mnemonic(unhexlify(data)))
 		return m
 
 	@staticmethod
-	def gen_mnen(strength):
+	def generate(strength,lang="english"):
 		#gen_mnemonic
 		m = key(m = mc(lang).generate(strength))
 		return m
@@ -28,14 +28,18 @@ class key(object):
 		seed = mc.to_seed(self.m, passphrase=passphrase)
 		xprv = BIP32Key.fromEntropy(seed).ExtendedKey() 
 		xpub = BIP32Key.fromEntropy(seed).ExtendedKey(private=False) 
+		#seed = self.b2h(seed)
 		return xprv,xpub
+
+	def seed(self,passphrase=""):
+		return mc.to_seed(self.m, passphrase=passphrase)
 
 	def cointype(self):
 		return None
 
 
 def main():
-	print(key.gen_seed(data="a9495fe923ce601f4394c8a7adadabc3").root_key())
+	print(key.to_mnemonic(data="a9495fe923ce601f4394c8a7adadabc3").root_key())
 
 if __name__ == '__main__':
 	main()

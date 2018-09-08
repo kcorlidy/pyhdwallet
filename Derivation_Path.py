1fb02aac2819a707621afd28855eb33add10d731
kcorlidy <1220181099@qq.com>
2018-09-08 22:57:35 +0800

    finished
    but i need to update a new version bip32utils,maybe tomorrow afternoon

--- Derivation_Path.py
+++ Derivation_Path.py
@@ -1,4 +1,4 @@
-from bip32utils import BIP32Key,Base58
+from bip32utils import BIP32Key
 from root_key import key
 from binascii import unhexlify,hexlify
 import sys
@@ -6,23 +6,26 @@ import sqlite3
 
 
 class bip44(object):
-	def __init__(self,e = None,path = None):
+	def __init__(self,e = None,path = None,bip=44,cointype="bitcoin",testnet=False):
 		self.e 				= self.warning() if e == None else e
 		self.path 			= path
 		self.BIP32_HARDEN 	= 0x80000000
 		self.k 				= None
 		self.accounts 		= {}
+		self.bip 			= bip
+		self.cointype		= cointype
+		self.testnet		= testnet
 
 	@staticmethod
-	def initialize(p,seed = None):
+	def initialize(p,seed = None,bip=44,cointype="bitcoin",testnet=False):
 		p = p.split("/")
-		state = False if p[0].lower() != "m" or p[1] not in ["44'","49'"] else True 
+		state = False if p[0].lower() != "m" or p[1] not in ["44'","49'","84'"] else True 
 		#if false mean user gave an unexpected path
 		if state == False:
 			raise Exception("Path error:please give a correct path")	
 		if seed == None:
 			print("If you do not specify a seed, you will use a random seed")		
-		bip = bip44(path=p,e=seed)
+		bip = bip44(path=p,e=seed,bip=bip,cointype=cointype,testnet=testnet)
 		bip.bip32ex_path()
 		return bip
 
@@ -36,10 +39,15 @@ class bip44(object):
 		return self.accounts
 
 	def address(self,k = None):
-		return self.k.Address() if k == None else k.Address()
+		if self.bip == 44:
+			return self.k.Address() if k == None else k.Address()
+		elif self.bip == 49:
+			return self.k.P2WPKHoP2SHAddress() if k == None else k.P2WPKHoP2SHAddress()
+		elif self.bip == 84:
+			return self.k.P2WPKHAddress() if k == None else k.P2WPKHAddress()
 
 	def bip32ex_path(self): 
-		k = BIP32Key.fromEntropy(self.e)
+		k = BIP32Key.fromEntropy(self.e,testnet=self.testnet)
 		for _,p in enumerate(self.path):
 			if "'" in p and p != 'm':
 				k = k.ChildKey(int(p.strip("'"))+self.BIP32_HARDEN)
@@ -55,7 +63,7 @@ class bip44(object):
 		
 	def exkey(self):
 		#BIP32 Extended Private/Public Key
-		return self.k.ExtendedKey(),self.k.ExtendedKey(private=False)
+		return self.k.ExtendedKey(bip=self.bip,cointype=self.cointype),self.k.ExtendedKey(private=False,bip=self.bip,cointype=self.cointype)
 
 	def cokey(self , k = None):
 		#Derived coin Private/Public Key
@@ -87,6 +95,7 @@ class bip44(object):
 			wif = self.wif(index)
 			address = self.address(index)
 			key = self.cokey(index)
+			print(address,key[1],wif,"\n")
 
 	def showpath(self,p):
 		return "".join([s+"/" for s in self.path])
@@ -115,6 +124,7 @@ class bip49(object):
 
 if __name__ == '__main__':
 	entropy = key.to_mnemonic(data="a9495fe923ce601f4394c8a7adadabc3").seed()
-	bip = bip44.initialize("m/49'/0'/0'/0",seed=entropy)
-	print(bip.exkey())
-	#bip.gen(5)
+	bip = bip44.initialize("m/84'/0'/0'/0",seed=entropy,bip=84)
+	#print(bip.exkey())
+	#print(bip.address())
+	bip.gen(5)

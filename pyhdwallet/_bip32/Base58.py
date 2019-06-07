cc2d00207424c14eac2eb7a63443f7817ee5fd5b
kcorlidy <1220181099@qq.com>
2019-06-07 17:31:06 +0800

    Update Base58.py

--- pyhdwallet/_bip32/Base58.py
+++ pyhdwallet/_bip32/Base58.py
@@ -78,7 +78,7 @@ def check_decode(enc):
     if chk != sha256(sha256(raw).digest()).digest()[:4]:
         raise ValueError("base58 decoding checksum error")
     else:
-        return raw
+        return raw[2:]
 
 
 class test(unittest.TestCase):
